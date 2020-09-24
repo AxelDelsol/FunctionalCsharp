0dc881cc8f48fa4a7e70b9aced6dc7b8ef2744cd
Axel Delsol <axel.delsol@laposte.net>
2020-09-24 11:38:03 +0200

    Created visual studio solution

---
+++ FunctionalCsharpSln/FunctionalCsharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FunctionalCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
